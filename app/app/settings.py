fb480e785ec667842d73ecd3e503ba36ea05adcf
Unknown <unknown@example.com>
2024-09-20 12:04:03 +0330

    change default user model and set postgres database to django

--- app/app/settings.py
+++ app/app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reservation',
+        'USER': 'postgres',
+        'PASSWORD': 'mossj77',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
@@ -123,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
