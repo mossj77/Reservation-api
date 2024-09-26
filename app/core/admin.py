276131c77bd71685fb291706d61369e26d115577
Unknown <unknown@example.com>
2024-09-26 11:04:59 +0330

    improve user admin panel

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,8 +1,22 @@
+from django.contrib.admin import ModelAdmin as BaseModelAdmin
 from django.contrib import admin
+from django.utils.translation import gettext as _
+
 from .models import *
 
 
-admin.site.register(User)
+class UserModelAdmin(BaseModelAdmin):
+    ordering = ['id']
+    list_display = ['email', 'f_name', 'l_name']
+    fieldsets = (
+        (_('Login Information'), {'fields': ('email', 'password')}),
+        (_('Personal Information'), {'fields': ('f_name', 'l_name', 'phone_number')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        (_('Dates'), {'fields': ('birth_date', 'last_login')}),
+    )
+
+
+admin.site.register(User, UserModelAdmin)
 admin.site.register(Hotel)
 admin.site.register(Room)
 admin.site.register(Reservation)
