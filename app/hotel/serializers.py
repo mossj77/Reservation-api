d1804725e1999dae9026f3a79a7d74de4ec90647
Unknown <unknown@example.com>
2024-10-03 10:43:31 +0330

    add update , delete and list view to hotel app

--- app/hotel/serializers.py
+++ app/hotel/serializers.py
@@ -1,10 +1,37 @@
 from rest_framework import serializers
 
-from core.models import Hotel
+from core.models import Hotel, Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'room_number', 'price', 'type')
 
 
 class HotelSerializer(serializers.ModelSerializer):
+    rooms = RoomSerializer(many=True)
 
     class Meta:
         model = Hotel
         fields = '__all__'
+
+    def create(self, validated_data):
+        rooms_data = validated_data.pop('rooms', None)
+        hotel = Hotel.objects.create(**validated_data)
+
+        for room_data in rooms_data:
+            Room.objects.create(hotel=hotel, **room_data)
+
+        return hotel
+
+    def update(self, instance, validated_data):
+        rooms_data = validated_data.pop('rooms', None)
+        hotel = super().update(instance, validated_data)
+
+        for room_data in rooms_data:
+            room_instance = Room.objects.get('id')
+            room_instance.update(room_data)
+            room_instance.save()
+
+        return hotel
--- app/hotel/views.py
+++ app/hotel/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets, mixins
 from .serializers import *
 
 
-class HotelViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
+class HotelViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin,
+                   mixins.DestroyModelMixin, mixins.UpdateModelMixin):
     serializer_class = HotelSerializer
     queryset = Hotel.objects.all()
